15eb778e1fb53ae4c01aa60dc95dbb3ca9f5d00e
devda <devdatwani235@gmail.com>
2023-12-15 11:05:38 +0530

    new

---
+++ Billing_Software/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.myprogress = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.percentagelb1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-3, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(496, 288)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(12, 245)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Loading..."
+        '
+        'myprogress
+        '
+        Me.myprogress.Location = New System.Drawing.Point(16, 261)
+        Me.myprogress.Name = "myprogress"
+        Me.myprogress.Size = New System.Drawing.Size(391, 11)
+        Me.myprogress.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.myprogress.TabIndex = 3
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 30
+        '
+        'percentagelb1
+        '
+        Me.percentagelb1.AutoSize = True
+        Me.percentagelb1.ForeColor = System.Drawing.Color.Red
+        Me.percentagelb1.Location = New System.Drawing.Point(73, 245)
+        Me.percentagelb1.Name = "percentagelb1"
+        Me.percentagelb1.Size = New System.Drawing.Size(15, 13)
+        Me.percentagelb1.TabIndex = 4
+        Me.percentagelb1.Text = "%"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(494, 282)
+        Me.Controls.Add(Me.percentagelb1)
+        Me.Controls.Add(Me.myprogress)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents myprogress As System.Windows.Forms.ProgressBar
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents percentagelb1 As System.Windows.Forms.Label
+
+End Class
---
+++ Billing_Software/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles myprogress.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        myprogress.Increment(1)
+        Dim percentage As String
+        percentage = Convert.ToString(myprogress.Value)
+        percentagelb1.Text = percentage + "%"
+        If myprogress.Value = 100 Then
+            Me.Hide()
+            Dim log = New frm_login
+            Timer1.Enabled = False
+            log.Show()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+
+End Class
